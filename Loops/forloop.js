a51170cfb826b01afdb36c728706c0a8a47b5dbc
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-06-08 02:18:17 +0530

    loop

--- Loops/forloop.js
+++ Loops/forloop.js
@@ -1,3 +1,7 @@
-for( let i = 0; i<34; i++) {
-    console.log(i)
+let sum = 0;
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+for( let i = 0; i<n; i++) {
+    sum += i
 }
+console.log("Sum of first " + n + " natural numbers is" + sum)
