f0a643e2de64155ed79a8e7266a69672857bcd20
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-06-10 00:49:23 +0530

    functions create

---
+++ functions/function.js
@@ -0,0 +1,14 @@
+function onePlusAvg(x , y){
+    // console.log("Done")
+    return 1 + (x + y)/2
+}
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+
+console.log("One Plus Average of a and b is ", onePlusAvg(a , b))
+console.log("One Plus Average of b and c is ", onePlusAvg(b , c))
+console.log("One Plus Average of a and c is ", onePlusAvg(a , c))
