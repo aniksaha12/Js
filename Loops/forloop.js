8f0f98385c0bbf4365f46e463ea4d1e145c93686
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-06-09 23:53:52 +0530

    For in and of loop

--- Loops/forloop.js
+++ Loops/forloop.js
@@ -1,5 +1,28 @@
 
-
-for( let i = 0; i<34; i++) {
-    console.log(i)
+let obj = {
+    harry:90,
+    subham:44,
+    shivika:80,
+    ritick:67,
+    shv:23,
 }
+
+/// for in loop
+for (let a in obj) {
+    console.log("Marks of " + a + " are " + obj[a])
+}
+
+/// for of loop
+for (let b of "harry"){
+    console.log(b)
+}
+///for loop
+
+let sum = 0
+let n = prompt("Enter the value of n")
+
+n = Number.parseInt(n)
+for (let i =0; i<n; i++){
+    sum += (i + 1)
+}
+console.log("Sum of first" +n + "natural numbers is " + sum)
