62643ce19b2e389183f3db87287283b713f78824
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-06-13 23:57:23 +0530

    practice set

---
+++ Chapter3/practice set3/Q3-1.js
@@ -0,0 +1,10 @@
+let marks = {
+    anik: 90,
+    subham: 9,
+    lavish: 56,
+    monika: 3
+}
+for(let i=0; i<Object.keys(marks).length; i++){
+    console.log("The marks of " + Object.keys(marks)[i]+ " are " + marks[Object.keys(marks)[i]])
+}
+
---
+++ Chapter3/practice set3/Q3-2.js
@@ -0,0 +1,12 @@
+
+// For in loop
+let marks = {
+    anik: 90,
+    subham: 9,
+    lavish: 56,
+    monika: 3
+}
+
+for(let Key in marks){
+    console.log("The marks of" + Key + " are " + marks[Key])
+}
