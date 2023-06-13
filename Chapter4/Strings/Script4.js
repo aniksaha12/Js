3d27bd84252f7c7d80e0b889f8331d988315cde7
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-06-14 00:43:48 +0530

    strings

---
+++ Chapter4/Strings/Script4.js
@@ -0,0 +1,17 @@
+let name = "ANIK"
+console.log(name.length)
+console.log(name[0])
+let friend = 'Rana'
+console.log(friend)
+
+
+let boy1 = "Harry"
+let boy2 = "Nikhil"
+
+let sentence = `${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+
+//  Escape Sequence Characters
+let fruit = 'Bana\'na'
+console.log(fruit)
