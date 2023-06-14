c61a4a3c4223c789502354f52d4fd7062e8eb9cd
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-06-15 00:42:48 +0530

    methods of strings

---
+++ Chapter4/Strings/Script5.js
@@ -0,0 +1,17 @@
+let name = "Anik"
+// console.log(name.length)
+
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+
+// console.log(name.slice(2,4))
+// console.log(name.replace("Ani","An"))
+
+// concat
+let friend = "Naman"
+console.log(name.concat(" is a friend of ", friend, "Ok"))
+
+let friend2 = "    Menna     "
+console.log(friend2)
+console.log(friend2.trim())
+
