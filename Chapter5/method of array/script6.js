99e4a16f4706ba92c5714eaaa2d9fa5682ca138f
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-06-19 01:45:22 +0530

    concat methods

--- Chapter5/method of array/script6.js
+++ Chapter5/method of array/script6.js
@@ -4,8 +4,32 @@ let n = [1, 3, 5, 7, 9];
 let b = n.toString(n)
 console.log(b)
 
-
-
 // Join method
 let c = n.join("_")
 console.log(c)
+
+// pop return the popped element
+
+let a = n.pop()
+console.log(n, a)
+
+// push return the new array length
+
+let p = n.push(10);
+console.log(n, p)
+
+// shift
+
+let r = n.shift()
+console.log(r, n)
+
+// unshift
+
+let u = n.unshift(55)
+console.log(u, n)
+
+// delete 
+
+let t = [90, 98, 78, 59, 3]
+let w = n.concat(t)
+console.log(t)
